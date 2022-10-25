e79630898468a6f1a9b85969dbb1e18ae69198e7
Grzegorz Domin <grze.domin98@gmail.com>
2022-10-25 23:42:07 +0200

    searchBar styled

--- src/SearchBar/styled.js
+++ src/SearchBar/styled.js
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
 export const Title = styled.h2`
-font-size: 26px;
-margin-top: 0;
-margin-bottom: 10px;
+  font-size: 26px;
+  margin-top: 0;
+  margin-bottom: 10px;
+  color: #454745;
 `;
 
 export const Input = styled.input`
-    min-width: 30%;
-    padding: 10px;
-    font-size: 14px;
-    border-radius: 3px;
-    border: 2px solid #e4e2e2;
+  min-width: 30%;
+  padding: 10px;
+  font-size: 14px;
+  border-radius: 3px;
+  border: 2px solid #6e6e6e;
 `;
