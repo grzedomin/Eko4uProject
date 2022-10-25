28abe9d199e88935c91096ad582f897f9bf75ab7
Grzegorz Domin <grze.domin98@gmail.com>
2022-10-25 23:36:00 +0200

    addUser saga added

--- src/Table/usersSaga.js
+++ src/Table/usersSaga.js
@@ -1,10 +1,11 @@
-import { takeEvery, put, takeLatest, select } from "redux-saga/effects";
+import { takeEvery, put, takeLatest, select, call } from "redux-saga/effects";
 import {
   fetchUsers,
   setUsers,
   removeUser,
   editUser,
-  selectUser,
+  selectUserJSON,
+  addNewUser,
 } from "./usersSlice";
 import axios from "axios";
 
@@ -18,35 +19,49 @@ function* fetchUsersHandler() {
 }
 
 function* deleteUserById(id) {
-  yield axios.delete(
-    `https://fronttest.ekookna.pl/user/${id.payload}`,
-    id.payload
-  );
+  yield axios({
+    url: `https://fronttest.ekookna.pl/user/${id.payload}`,
+    method: "delete",
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Content-type": "application/json",
+      "Access-Control-Allow-Methods": "DELETE",
+    },
+  });
   yield put(removeUser(id));
 }
 
-function* updateUser(id) {
-  const userBody = yield select(selectUser);
-
+function* updateUser(user) {
+  const id = user.payload.id;
+  const userBody = yield select(selectUserJSON);
   try {
-    const response = yield axios.put(
-      `https://fronttst.ekookna.pl/user/${id}`,
-      userBody
-    );
-    yield console.log(response, "xx");
-    yield put(setUsers(response.data.users));
+    yield axios({
+      url: `https://fronttst.ekookna.pl/user/${id}`,
+      headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+      },
+      method: "put",
+      data: userBody,
+    });
+    yield call(fetchUsers);
   } catch (error) {
     yield console.log(error, "error update");
   }
 }
 
-// function* addUser(user) {
-//   yield axios.post("fronttest.ekookna.pl/user", user);
-
-// }
+function* addUser() {
+  const newUserbody = yield select(selectUserJSON);
+  try {
+    yield axios.post(`https://fronttst.ekookna.pl/user/`, newUserbody);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export function* usersSaga() {
   yield takeEvery(fetchUsers.type, fetchUsersHandler);
   yield takeEvery(removeUser.type, deleteUserById);
   yield takeLatest(editUser.type, updateUser);
+  yield takeEvery(addNewUser.type, addUser);
 }
